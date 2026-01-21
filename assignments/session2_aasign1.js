7a3a784430d4818c53bcb3cbb23df9a7e4bd1473
patel-neil <neilpatel400271@gmail.com>
2026-01-21 11:25:19 +0530

    assignment1 of Session2 done

---
+++ assignments/session2_aasign1.js
@@ -0,0 +1,16 @@
+//Code to check whether the given number is positive,negative or zero
+
+let input_number = Number(prompt("Enter a number:"));
+
+if(input_number > 0)
+{
+    console.log("The number is positive.");
+}
+else if(input_number < 0)
+{
+    console.log("The number is negative.");
+}
+else
+{
+    console.log("The number is zero.");
+}
