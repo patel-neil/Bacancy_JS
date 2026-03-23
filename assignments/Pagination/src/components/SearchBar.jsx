18b127b61fdd8accea954518fc787dfdb3470191
patel-neil <neilpatel400271@gmail.com>
2026-03-23 11:09:25 +0530

    Completed tasks on Pagination, Searching using Debounce Custom Hook and Sorting.

---
+++ assignments/Pagination/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+// components/SearchBar.js
+export default function SearchBar({ search, setSearch }) {
+  return (
+    <input
+      type="text"
+      placeholder="Search products..."
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+    />
+  );
+}
